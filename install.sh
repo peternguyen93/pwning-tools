4af2518ea07b0728218e63feca831c5ee0906ccb
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2015-04-16 13:51:53 +0700

    Updated Installed Script"

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #/bin/sh
+PYTHON2="/usr/lib/python2.7"
+PYTHON3="/usr/lib/python3.4"
+PYTHON=""
 
-sudo cp Shellcode.py Pwning.py /usr/lib/python2.7
+if [ -d "$PYTHON2" ]; then
+	echo "Found Python 2"
+	PYTHON="$PYTHON2"
+elif [ -d "$PYTHON3" ]; then
+	echo "Found Python 3"
+	PYTHON="$PYTHON3"
+fi
+
+sudo cp Shellcode.py Pwning.py $PYTHON
