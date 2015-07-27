7b96092b3f5dafc5ff0b087f4a2a68ffd74dde25
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2015-07-27 14:00:23 +0700

    Fix some problem

--- Templet.py
+++ Templet.py
@@ -4,19 +4,21 @@ from Pwn import *
 # when binary doesn't have nx eable or mprotect/nmap is on got table
 # from Shellcode import *
 
-exp = Pwn(host='example.com',port=8888)
+p = Pwn(host='example.com',port=8888)
+# if you want to pwn local
+p = Pwn(isconnect=False)
 
-def exploit():
-	# some exploit code
-	exp.p(0xcafebabe)
-	exp.write('A'*4)
+def ploit():
+	# some ploit code
+	p.pack(0xcafebabe)
+	p.write('A'*4)
 
-	write_addr = exp.up(exp.recv(4))
+	write_addr = p.unpack(p.recv(4))
 	system_addr = write_addr - offset
 
 	print '[+] write() :',hex(write_addr)
 	print '[+] system() :',hex(system_addr)
 
-	exp.io()
+	p.io()
 
 exploit()
