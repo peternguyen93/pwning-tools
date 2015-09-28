f1e2ef2d368d09b95564cca96a408e8805712189
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2015-09-28 18:56:09 +0700

    fix install script only adapt to Linux

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 PYTHON2="/usr/lib/python2.7"
 PYTHON3="/usr/lib/python3.4"
 PYTHON=""
+os_name="$(uname -s)"
 
 if [ -d "$PYTHON2" ]; then
 	echo "Found Python 2"
@@ -13,5 +14,8 @@ else
 	exit 1
 fi
 sudo cp Shellcode.py Pwn.py $PYTHON
-sudo cp gdb_attach /usr/bin/
-sudo cp sct /usr/bin/
+
+if [ "Linux" == "$os_name" ]; then
+	sudo cp gdb_attach /usr/bin/
+	sudo cp sct /usr/bin/
+fi
