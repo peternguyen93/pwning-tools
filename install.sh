c607cdc4ad22a762d5d693116cc1fc80d27c3e7c
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2015-10-01 13:04:49 +0700

    Fix install script for Mac OS X 10.11 El Capital

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ PYTHON3="/usr/lib/python3.4"
 PYTHON=""
 os_name="$(uname -s)"
 
-if [ -d "$PYTHON2" ]; then
-	echo "Found Python 2"
-	PYTHON="$PYTHON2"
-elif [ -d "$PYTHON3" ]; then
-	echo "Found Python 3"
-	PYTHON="$PYTHON3"
-else
-	exit 1
-fi
-sudo cp Shellcode.py Pwn.py $PYTHON
-
 if [ "Linux" = "$os_name" ]; then
+	if [ -d "$PYTHON2" ]; then
+		echo "Found Python 2"
+		PYTHON="$PYTHON2"
+	elif [ -d "$PYTHON3" ]; then
+		echo "Found Python 3"
+		PYTHON="$PYTHON3"
+	else
+		exit 1
+	fi
+
+	sudo cp Shellcode.py Pwn.py $PYTHON
 	sudo cp gdb_attach /usr/bin/
 	sudo cp sct /usr/bin/
+elif [ "Darwin" = "$os_name" ]; then
+	sudo cp Shellcode.py Pwn.py /Library/Python/2.7/site-packages/
 fi
