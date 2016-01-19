325fe3b69d4aaddd48ec5f6859ef7b3f21442b68
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2016-01-19 15:42:20 +0700

    version 0.5

--- Pwn.py
+++ Pwn.py
@@ -15,10 +15,11 @@
 # Requires: pyelftools
 
 # Author : Peternguyen
-# Version : 0.3.2
+# Version : 0.5
 
 # /bin/sh -c "echo shell >&4; sh <&4 >&4"
 
+from __future__ import print_function
 from ctypes import *
 from struct import *
 from elftools.common.py3compat import bytes2str
@@ -30,11 +31,11 @@ import os
 import json
 import urllib2
 import urllib
-import random
 import string
 
 LIBC_REPO = 'http://libc.babyphd.net/' # own libc repo
 
+# use this key to authen with libc.babyphd.net
 def load_auth_key():
 	default_path = os.path.expanduser('~/.libc_collection_authkey')
 	try:
@@ -46,9 +47,41 @@ def load_auth_key():
 			key = json.load(file_handle)['authkey']
 			file_handle.close()
 			return key
-	except:
-		pass
+	except KeyError :
+		print('"authkey" doesn\'t exists')
 	return None
+
+class ELFTable(dict):
+	def __init__(self,*arg,**kw):
+		super(ELFTable, self).__init__(*arg, **kw)
+	# override this method to perform new search method act like this example:
+	# >>> got = ELFTable({
+	# ...             '__gmon_start__': 6294744,
+	# ...             '_IO_getc': 6294752,
+	# ...             'puts': 6294704,
+	# ...             '__printf_chk': 6294760,
+	# ...             'memset': 6294728
+	# ... })
+	# >>> got['getc'] # will return '_IO_getc' value
+	# 6294752
+	def __getitem__(self,key):
+		for _key in self.keys():
+			if key in _key:
+				return dict.__getitem__(self,_key)
+		raise KeyError('%s is\'t found' % key)
+	
+	# istead using key lookup, user can lookup value as a function
+	# >>> got = ELFTable({
+	# ...             '__gmon_start__': 6294744,
+	# ...             '_IO_getc': 6294752,
+	# ...             'puts': 6294704,
+	# ...             '__printf_chk': 6294760,
+	# ...             'memset': 6294728
+	# ... })
+	# >>> got('getc') # will return '_IO_getc' value
+	def __call__(self,arg):
+		return self.__getitem__(arg)
+
 
 class Telnet(telnetlib.Telnet):
 	def __init__(self,host,port):
@@ -67,8 +100,12 @@ class Pwn():
 		self.host = 'localhost'
 		self.port = 8888
 		self.con = None
-		self.pfile = None
 		self.sock_file = None
+		self.got = ELFTable({})
+		self.plt = ELFTable({})
+
+		# load authkey
+		self.authkey = load_auth_key()
 
 		# user inputs values
 		for key,value in kwargs.iteritems():
@@ -88,18 +125,66 @@ class Pwn():
 					self.port = value
 				else:
 					raise Exception('Unexpected value of self.port')
-			elif key.lower() == 'pfile':
+			elif key.lower() == 'elf':
+				# define new input to dump got and plt table in elf file
+				# >>> p = Pwn(elf='pwn.elf')
+				# >>> p.got('puts')
+				# 0x60008
+				# >>> p.plt('puts')
+				# 0x400640
+				# >>> p.got['puts']
+				# 0x60008
+				# >>> p.plt['puts']
+				# 0x400640
 				if type(value) is str:
-					if os.path.exists(value):
-						self.pfile = open(value,'r')
-					else:
-						raise Exception('File %s not found' % value)
+					path = os.path.expanduser(value) # ~/
+					# is elf file exists
+					if not os.path.exists(path):
+						raise Exception('File %s not found' % path)	
+					self.__elfparsing(path)
+
+	def __elfparsing(self,path):
+		# parsing elf file to dump got and plt table
+		with open(path,'r') as pfile:
+			elf = ELFFile(pfile)
+			if not elf:
+				raise Exception('File %s is not elf file' % path)
+
+			# get rellocation section 
+			arch = elf.get_machine_arch() # get arch
+			# auto set self.mode base on arch of elf binary
+			self.mode = 0 if arch == 'x86' else 1
+
+			plt_section = elf.get_section_by_name('.plt')
+			plt_address = plt_section.header['sh_addr'] # get plt base address
+			entry_align = plt_section.header['sh_addralign'] # plt entry size
+
+			# dump symbol table
+			symbol_sec = elf.get_section_by_name(b'.dynsym')
+			# can dump ?
+			if not isinstance(symbol_sec, SymbolTableSection):
+				raise Exception('Can dump SymbolTableSection')
+
+			# get reallocation section to dump got table
+			reladyn_name = b'.rel.plt' if arch == 'x86' else b'.rela.plt'
+			reladyn = elf.get_section_by_name(reladyn_name)
+			# can dump ?
+			if not isinstance(reladyn, RelocationSection):
+				raise Exception('Can dump SymbolTableSection')
+
+			# dumping got and plt table
+			for reloc in reladyn.iter_relocations():
+				plt_address += entry_align
+				got_func_name = symbol_sec.get_symbol(reloc['r_info_sym']).name
+				# dumping SymbolTableSection
+				self.got[got_func_name] = reloc['r_offset']
+				# mapping plt base address with SymbolTableSection
+				self.plt[got_func_name] = plt_address
 
 	def connect(self):
-		if not self.con:
-			self.con = Telnet(self.host,self.port)
-		else:
+		if self.con:
 			raise Exception('You had connected.')
+		self.con = Telnet(self.host,self.port)
 
 	# make socket as file, use with libncurse in service
 	def makefile(self,mode,bufsize=0): # default bufsize = 0 unbuffered
@@ -122,9 +207,8 @@ class Pwn():
 	# wrapper popular send/recive function
 	def read_until(self,value):
 		if self.con:
-			return self.con.read_until(value)
-		else:
 			raise Exception('You must connect() first')
+		return self.con.read_until(value)
 
 	def read_all(self):
 		rc = self.con.recv(1024)
@@ -137,70 +221,40 @@ class Pwn():
 
 	def write(self,value):
 		if self.con:
-			return self.con.write(value)
-		else:
 			raise Exception('You must connect() first')
+		return self.con.write(value)
 
 	def send(self,value):
 		if self.con:
-			return self.con.send(value)
-		else:
 			raise Exception('You must connect() first')
+		return self.con.send(value)
 
 	def recv(self,size):
 		if self.con:
-			return self.con.recv(size)
-		else:
 			raise Exception('You must connect() first')
+		return self.con.recv(size)
 
 	def close(self):
 		if self.con:
-			self.con.close()
-		else:
 			raise Exception('You must connect() first')
+		self.con.close()
 
 	def io(self):
-		print '[+] Pwned Shell.'
+		print('[+] Pwned Shell.')
 		self.con.interact()
-
-	# get base libc address from leak address.
-	# >>> from Pwn import *
-	# >>> p = Pwn()
-	# >>> p.get_libc_base_addr('puts',0x7ffff7a84e30)
-	# 140737347932160
-	def get_libc_base_addr(self,func_name,func_addr):
-		offset = 0
-		authkey = load_auth_key()
-		if not authkey:
-			raise Exception('You must set your authkey in ~/.libc_collection_authkey to use this method')
-		try:
-			form = { # own post request
-				'leak_addr' : hex(func_addr),
-				'func_name' : func_name,
-				'auth' : authkey
-			}
-			headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
-			req = urllib2.Request(LIBC_REPO + 'get_libc_base_addr',urllib.urlencode(form),headers) # getting result
-			res = urllib2.urlopen(req)
-			result = json.loads(res.read())
-			res.close()
-
-			offset = result['libc_base_addr']
-		except: # handle every exception
-			pass
-		return offset
 
 	# get offset between to function if you can leak one of them
 	# >>> from Pwn import *
 	# >>> p = Pwn()
-	# >>> offset = p.get_libc_offset(0x7ffff7a84e30,'puts')
+	# >>> offset,base_address = p.get_libc_offset(0x7ffff7a84e30,'puts')
 	# >>> print hex(offset)
 	# 0x297f0
+	# >>> print hex(base_address)
+	# 0x7ffff7a15000
 	def get_libc_offset(self,func2_addr,func2_name,func1_name='system'):
 		# get offset on own collection
-		offset = 0
-		authkey = load_auth_key()
-		if not authkey:
+		offset = base_addr = 0
+		if not self.authkey:
 			raise Exception('You must set your authkey in ~/.libc_collection_authkey to use this method')
 
 		try:
@@ -208,7 +262,7 @@ class Pwn():
 				'func_addr' : hex(func2_addr),
 				'func_name' : func2_name,
 				'func2_name' : func1_name,
-				'auth' : authkey
+				'auth' : self.authkey
 			}
 			headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 			req = urllib2.Request(LIBC_REPO + 'libc_find',urllib.urlencode(form),headers) # getting result
@@ -217,39 +271,11 @@ class Pwn():
 			res.close()
 
 			offset = result['offset']
+			base_addr = result['base_addr']
 		except: # handle every exception
 			pass
-		return offset
-
-	# get all possible offset by os_name os_version and arch
-	# >>> loff = p.get_libc_offset_by('system','ubuntu','14.04','i386')
-	# >>> print loff
-	# [261328, 262544, 261904, 263088]
-	def get_libc_offset_by(self,func_name,os_name,os_version,arch):
-		offset = None
-		authkey = load_auth_key()
-		if not authkey:
-			raise Exception('You must set your authkey in ~/.libc_collection_authkey to use this method')
-
-		try:
-			form = { # own post request
-				'func_name' : func_name,
-				'os_name' : os_name,
-				'os_version' : os_version,
-				'arch' : arch,
-				'auth' : authkey
-			}
-			headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
-			# getting result
-			req = urllib2.Request(LIBC_REPO + 'get_offset_by_os_name',urllib.urlencode(form),headers)
-			res = urllib2.urlopen(req)
-			result = json.loads(res.read())
-			res.close()
-
-			offset = result['offset']
-		except: # handle every exception
-			pass
-		return offset
+		# return both offset between 2 function and base address of libc
+		return offset,base_addr
 
 	# this method helps you calc local libc.so offset
 	# >>> offset = p.calc_libc_offset('/lib/x86_64-linux-gnu/libc.so.6','puts')
@@ -258,67 +284,30 @@ class Pwn():
 	def calc_libc_offset(self,libc_path,func2,func1='system'):
 		offset = 0
 		if os.path.exists(libc_path):
-			pfile = open(libc_path,'r')
+			with open(libc_path,'r') as pfile:
+				# can't find any thing calculate it
+				elffile = ELFFile(pfile)
 
-			# can't find any thing calculate it
-			elffile = ELFFile(pfile)
+				# dump symbol table
+				symbol_sec = elffile.get_section_by_name(b'.dynsym')
+				# can dump ?
+				if not isinstance(symbol_sec, SymbolTableSection):
+					return None
 
-			# dump symbol table
-			symbol_sec = elffile.get_section_by_name(b'.dynsym')
-			# can dump ?
-			if not isinstance(symbol_sec, SymbolTableSection):
-				return None
+				func1_addr = 0
+				func2_addr = 0
+				for symbol in symbol_sec.iter_symbols():
+					if symbol.name == func1:
+						func1_addr = symbol.entry['st_value'] # get offset of func1
+					if symbol.name == func2:
+						func2_addr = symbol.entry['st_value'] # get offset of func2
+					# collect all neccessary function
+					if func1_addr and func2_addr:
+						break
 
-			func1_addr = 0
-			func2_addr = 0
-			for symbol in symbol_sec.iter_symbols():
-				if symbol.name == func1:
-					func1_addr = symbol.entry['st_value'] # get offset of func1
-				if symbol.name == func2:
-					func2_addr = symbol.entry['st_value'] # get offset of func2
-				# collect all neccessary function
-				if func1_addr and func2_addr:
-					break
-
-			offset = func1_addr - func2_addr if func1_addr > func2_addr else func2_addr - func1_addr
-
-			pfile.close()
+				offset = func1_addr - func2_addr
 
 		return offset
-
-	# easy way to find got :v
-	# >>> p = Pwn(pfile='pwn.elf')
-	# >>> p.got('puts')
-	# 0x60008
-	def got(self,func_name):
-		func_addr = 0
-
-		if self.pfile:
-			elffile = ELFFile(self.pfile)
-
-			arch = elffile.get_machine_arch() # get arch
-
-			# dump symbol table
-			symbol_sec = elffile.get_section_by_name(b'.dynsym')
-			# can dump ?
-			if not isinstance(symbol_sec, SymbolTableSection):
-				return None
-
-			# get reallocation section to dump got table
-			reladyn_name = b'.rel.plt' if arch == 'x86' else b'.rela.plt'
-			reladyn = elffile.get_section_by_name(reladyn_name)
-			# can dump ?
-			if not isinstance(reladyn, RelocationSection):
-				return None
-
-			# find function address in got table
-			for reloc in reladyn.iter_relocations():
-				got_func_name = symbol_sec.get_symbol(reloc['r_info_sym']).name
-				if got_func_name == func_name:
-					func_addr = reloc['r_offset']
-					break
-
-		return func_addr # adding packing before return value
 
 	# utilities method that support you make your payload easier
 	def p32(self,value):
@@ -400,32 +389,82 @@ class Pwn():
 			buf += b
 		return buf
 
-	# random string,number
-	def rand_string(self,N):
-		return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+	# string cyclic function
+	# this code base on https://github.com/Gallopsled/pwntools/blob/master/pwnlib/util/cyclic.py
+	# Taken from https://en.wikipedia.org/wiki/De_Bruijn_sequence but changed to a generator
+	def de_bruijn(self, alphabet = string.ascii_lowercase, n = 4):
+		"""de_bruijn(alphabet = string.ascii_lowercase, n = 4) -> generator
 
-	#xxd hexdump function
-	def xxd(self,stream):
-		_str = ""
-		i = 0
-		for i,s in enumerate(list(stream)):
-			if i%16 == 0:
-				print "%07x:" % i,
-			print "%02x" % ord(s),
-			_str += s if( ord(s) in range(0x20,0x7f) ) else "."
-			if (i+1)%8 == 0:
-				print "",
-			if (i+1)%16 == 0:
-				print "|  %s" % _str
-				_str = ""
-		if (i+1)%16 != 0:
-			print "   "*(16-((i%16)+1)),
-			if (16-((i%16)+1)) > 8:
-				print "",
-			print "|  %s" % _str
+		Generator for a sequence of unique substrings of length `n`. This is implemented using a
+		De Bruijn Sequence over the given `alphabet`.
 
+		The returned generator will yield up to ``len(alphabet)**n`` elements.
 
-	# deallocation object
-	def __del__(self):
-		if self.pfile:
-			self.pfile.close()
+		Arguments:
+		  alphabet: List or string to generate the sequence over.
+		  n(int): The length of subsequences that should be unique.
+		"""
+		k = len(alphabet)
+		a = [0] * k * n
+		def db(t, p):
+			if t > n:
+				if n % p == 0:
+					for j in range(1, p + 1):
+						yield alphabet[a[j]]
+			else:
+				a[t] = a[t - p]
+				for c in db(t + 1, p):
+					yield c
+
+				for j in range(a[t - p] + 1, k):
+					a[t] = j
+					for c in db(t + 1, t):
+						yield c
+
+		return db(1,1)
+
+	# generate a cyclic string
+	def cyclic(self, length = None, n = 4):
+		alphabet = string.printable[:-6]# default charset
+
+		out = []
+		for ndx, c in enumerate(self.de_bruijn(alphabet, n)):
+			if length != None and ndx >= length:
+				break
+			else:
+				out.append(c)
+
+		if isinstance(alphabet, str):
+			return ''.join(out)
+		else:
+			return out
+
+	# finding subseq in generator then return pos of this subseq
+	# if it doens't find then return -1
+	def cyclic_find(self, subseq, length = 0x10000):
+		generator = self.cyclic(length)
+
+		if isinstance(subseq, (int, long)): # subseq might be a number or hex value
+			try:
+				subseq = self.p32(subseq)
+			except error: # struct.error
+				try:
+					subseq = self.p64(subseq)
+				except error: # struct.error
+					return -1
+		
+		if not isinstance(subseq,str):
+			return -1
+		# finding position of subseq
+		subseq = list(subseq)
+		saved = []
+		pos = 0
+
+		for c in generator:
+			saved.append(c)
+			if len(saved) > len(subseq):
+				saved.pop(0)
+				pos += 1
+			if saved == subseq: # if subseq equal saved then return pos of subseq
+				return pos
+		return -1
