254699f4202e0a61c040e529a14fe520652ac424
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2016-02-24 14:41:15 +0700

    update

--- install.sh
+++ install.sh
@@ -18,6 +18,10 @@ if [ "Linux" = "$os_name" ]; then
 	sudo cp Shellcode.py Pwn.py $PYTHON
 	sudo cp gdb_attach /usr/bin/
 	sudo cp sct /usr/bin/
+	sudo mkdir -p /usr/local/lib/libstdlib32/
+	sudo cp stdbuf32/libstdbuf.so /usr/local/lib/libstdlib32/
+	sudo cp stdbuf32/stdbuf32.sh /usr/local/bin/stdbuf32
+	
 elif [ "Darwin" = "$os_name" ]; then
 	sudo cp Shellcode.py Pwn.py /Library/Python/2.7/site-packages/
 fi
